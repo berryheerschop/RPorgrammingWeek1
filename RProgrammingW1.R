9b6622f17323e3388e2937ac277487fc143bd49d
berryheerschop <berryheerschop@gmail.com>
2019-11-14 16:36:22 +0100

    Added new example code

--- RProgrammingW1.R
+++ RProgrammingW1.R
@@ -2,10 +2,34 @@
 
 getwd()
 
-# Define a variable x
+#define a new variable x
 
 x <- 1
 
-#Print the value of x
+# Print the value of x
 
 print(x)
+
+# Define an integer sequence variable
+
+y <- 1:20
+
+#autoprint y
+
+y
+
+#Divide by Infinity
+
+1/ Inf
+
+# Show difference between a numeric and an integer
+z <- 1
+w <- 1L
+
+z
+w
+
+class(z)
+class(w)
+
+
